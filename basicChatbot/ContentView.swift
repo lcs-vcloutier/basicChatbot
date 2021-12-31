70a0d877280749a9a7ab940ab6b081f828d4a5fd
lcs-vcloutier <vcloutier@lcs.on.ca>
2021-12-31 17:10:54 -0500

    Initial Commit

---
+++ basicChatbot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  basicChatbot
+//
+//  Created by Vincent Cloutier on 2021-12-31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ basicChatbot/basicChatbotApp.swift
@@ -0,0 +1,17 @@
+//
+//  basicChatbotApp.swift
+//  basicChatbot
+//
+//  Created by Vincent Cloutier on 2021-12-31.
+//
+
+import SwiftUI
+
+@main
+struct basicChatbotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
