83e90a493bb1ddc413502848cf01ae0fdbd33d19
lcs-vcloutier <vcloutier@lcs.on.ca>
2022-01-01 20:55:45 -0500

    setup arrays for more replies

--- basicChatbot/getBotResponse.swift
+++ basicChatbot/getBotResponse.swift
@@ -14,33 +14,55 @@ func getBotResponse(message: String) -> String {
     var realSentiment = ""
     let tagger = NLTagger(tagSchemes: [.tokenType, .sentimentScore])
     tagger.string = text
-
+    
     tagger.enumerateTags(in: text.startIndex..<text.endIndex, unit: .paragraph,
                          scheme: .sentimentScore, options: []) { sentiment, _ in
         
         if let sentimentScore = sentiment {
             realSentiment = sentimentScore.rawValue
-
+            
             print(sentimentScore.rawValue)
         }
         return true
     }
-
-
+    
+    
     let sentimentDouble = Double(realSentiment)!
+    let neutralReplies = [
+        "Please try to keep this conversation focused on events or feelings. I'm just your neighborhood shrink after all.",
+        "neutral 2",
+        "neutral 3",
+        "neutral 4",
+        "neutral 5"
+    ]
+    let positiveReplies = [
+        "That's great! I've seen it all so feel free to lay the hard stuff on me to.",
+        "positive 2",
+        "positive 3",
+        "positive 4",
+        "positive 5",
+        "positive 6",
+        "positive 7",
+        "positive 8",
+        "positive 9",
+        "positive 10",
+        "positive 11",
+        "positive 12",
+        "positive 13",
+    ]
+    let negativeReplies = [
+        "Wow that's just terrible. Please expand on this.",
+        "negative 2",
+        "negative 3",
+        "negative 4",
+        "negative 5"
+    ]
     
     if sentimentDouble == 0.0 {
-        return "Please try to keep this conversation focused on events or feelings. I'm just your neighborhood shrink after all."
+        return neutralReplies.randomElement()!
     } else if sentimentDouble > 0.1 {
-        return "That's great! I've seen it all so feel free to lay the hard stuff on me to."
-    } else if sentimentDouble > -0.5 {
-        return "That's pretty bad. Tell me more."
+        return positiveReplies.randomElement()!
     } else {
-        return "Wow that's just terrible. Please expand on this."
+        return negativeReplies.randomElement()!
     }
 }
-
-
-
-
-///-> need to add more in depth information about the user and more responses also need to clean up code
