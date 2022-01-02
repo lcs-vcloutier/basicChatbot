7d06808bb95eeb40b70c1188858e853c26268bc3
lcs-vcloutier <vcloutier@lcs.on.ca>
2022-01-02 15:21:51 -0500

    added app icon

--- basicChatbot/ContentView.swift
+++ basicChatbot/ContentView.swift
@@ -43,7 +43,7 @@ struct ContentView: View {
                 }
                 .padding()
             }
-            .navigationTitle("Shrink")
+            .navigationTitle("Truncate")
         }
     }
     func postMessage(msg: String) {
