5839ef95c7e8ff421729282b03c4aaf77640b5ac
lcs-vcloutier <vcloutier@lcs.on.ca>
2022-01-04 10:56:46 -0500

    setup frame for resources page. will be adding content now

--- basicChatbot/ResourceView.swift
+++ basicChatbot/ResourceView.swift
@@ -9,12 +9,65 @@ import SwiftUI
 
 struct ResourceView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            Text("This app is not a replacement for professional psychological help. Below are links that help explain how we developed the chatbot's answers. There are also links to professional resources.")
+                .font(.title)
+            Button {
+                // Open Link
+            } label: {
+                CardView(image: "Image", category: "test", heading: "test", author: "test")
+            }
+            Button {
+                // Open Link
+            } label: {
+                CardView(image: "Image", category: "test", heading: "test", author: "test")
+            }
+            Button {
+                // Open Link
+            } label: {
+                CardView(image: "Image", category: "test", heading: "test", author: "test")
+            }
+
+            Spacer()
+        }
+        .navigationTitle("Resources")
+        .padding()
     }
 }
 
-struct ResourceView_Previews: PreviewProvider {
-    static var previews: some View {
-        ResourceView()
+
+struct CardView: View {
+    var image: String
+    var category: String
+    var heading: String
+    var author: String
+    var body: some View {
+        VStack {
+            Image(image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+
+            HStack {
+                VStack(alignment: .leading) {
+                    Text(category)
+                        .font(.headline)
+                        .foregroundColor(.secondary)
+                    Text(heading)
+                        .font(.title)
+                        .fontWeight(.black)
+                        .foregroundColor(.primary)
+                        .lineLimit(3)
+                    Text(author.uppercased())
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                }
+                .layoutPriority(100)
+
+                Spacer()
+            }
+            .padding()
+        }
+        
     }
 }
+
